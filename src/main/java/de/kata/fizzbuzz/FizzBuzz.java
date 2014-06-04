6075915538c99e53b27f24063c0c770c5ad0f24f
Markus Hanses <me@markushanses.de>
2014-06-04 19:15:44 +0200

    Implementation code reduced to minimum.

--- src/main/java/de/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/de/kata/fizzbuzz/FizzBuzz.java
@@ -3,24 +3,11 @@ package de.kata.fizzbuzz;
 public class FizzBuzz {
 
     public String say(int i) {
+        return isFizzBuzz(i) ? "FizzBuzz" : isFizz(i) ? "Fizz" : isBuzz(i) ? "Buzz" : String.valueOf(i);
+    }
 
-        StringBuilder builder = new StringBuilder();
-
-        boolean isFizz = isFizz(i);
-        if (isFizz) {
-            builder.append("Fizz");
-        }
-
-        boolean isBuzz = isBuzz(i);
-        if (isBuzz) {
-            builder.append("Buzz");
-        }
-
-        if(!isFizz && ! isBuzz) {
-            builder.append(String.valueOf(i));
-        }
-
-        return builder.toString();
+    private boolean isFizzBuzz(int i) {
+        return isFizz(i) && isBuzz(i);
     }
 
     private boolean isBuzz(int i) {
