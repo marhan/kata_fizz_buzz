03fe147f5d5c8b40d985e8dcd9f244dd7ae48e3f
Markus Hanses <me@markushanses.de>
2014-06-18 17:22:43 +0200

    Clean implementations. Guidingtest commented out.

--- src/main/java/de/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/de/kata/fizzbuzz/FizzBuzz.java
@@ -8,36 +8,5 @@ import java.util.TreeMap;
 
 public class FizzBuzz {
 
-    private TreeMap<Integer, String> replacements = new TreeMap<>();
 
-    public FizzBuzz() {
-        replacements.put(7, "Whizz");
-        replacements.put(15, "FizzBuzz");
-        replacements.put(3, "Fizz");
-        replacements.put(5, "Buzz");
-    }
-
-    public String say(Integer number) {
-
-        if(isReplaceable(number)) {
-           return replace(number);
-        }
-
-        return String.valueOf(number);
-    }
-
-    private String replace(Integer number) {
-
-        for(Integer key : replacements.descendingKeySet()){
-            if(number % key == 0) {
-               return replacements.get(key);
-            }
-        }
-
-        return null;
-    }
-
-    private boolean isReplaceable(Integer number) {
-        return replace(number) != null;
-    }
 }
--- src/test/java/de/kata/fizzbuzz/FizzBuzzGuidingTest.java
+++ src/test/java/de/kata/fizzbuzz/FizzBuzzGuidingTest.java
@@ -1,31 +1,60 @@
 package de.kata.fizzbuzz;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FizzBuzzGuidingTest {
+    /*
 
-    private FizzBuzz fizzBuzz;
+    private FizzBuzz fizzBuzz = new FizzBuzz();
 
-    @Before
-    public void setup() {
-        fizzBuzz = new FizzBuzz();
-    }
+    private List<Integer> expectedAsNumber = Arrays.asList(1, 2, 4, 8, 11, 13, 16, 17, 19);
 
-/*
+    private List<Integer> expectedAsFizz = Arrays.asList(3, 6, 9, 12, 18);
+
+    private List<Integer> expectedAsBuzz = Arrays.asList(5, 10, 20);
+
+    private List<Integer> expectedAsFizzBuzz = Arrays.asList(15, 30, 45);
+
+    private List<Integer> expectedAsWhizz = Arrays.asList(7, 14, 21, 28);
+
     @Test
-    public void iterateOverTestData() {
-        GuidingTestData guidingTestData = new GuidingTestData();
-        guidingTestData.prepareForIteration3();
-
-        for (int i = 1; i <= guidingTestData.getHighestNumber(); i++) {
-            String expected = guidingTestData.getResultFor(i);
-            String output = fizzBuzz.say(i);
-            assertThat(String.format("Expectation failed for %s", i), output, is(expected));
+    public void numbersExpected() {
+        for (Integer number : expectedAsNumber) {
+            assertThat(fizzBuzz.say(number), is(String.valueOf(number)));
         }
     }
-*/
+
+    @Test
+    public void fizzExpected() {
+        assertNumbers(expectedAsFizz, "Fizz");
+    }
+
+    @Test
+    public void buzzExpected() {
+        assertNumbers(expectedAsBuzz, "Buzz");
+    }
+
+    @Test
+    public void fizzBuzzExpected() {
+        assertNumbers(expectedAsFizzBuzz, "FizzBuzz");
+    }
+
+    @Test
+    public void whizzExpected() {
+        assertNumbers(expectedAsWhizz, "Whizz");
+    }
+
+    private void assertNumbers(List<Integer> numbers, String expectation) {
+        for (Integer number : numbers) {
+            assertThat(fizzBuzz.say(number), is(expectation));
+        }
+    }
+
+    */
 }
